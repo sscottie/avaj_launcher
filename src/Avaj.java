17d425bdca36c3143bf9c7b8ef114a7be36599a7
Sherry Scottie <sscottie@ox-p5.msk.21-school.ru>
2021-03-13 16:39:23 +0300

    added schema and main func

--- src/Avaj.java
+++ src/Avaj.java
@@ -1,12 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Main.java                                          :+:      :+:    :+:   */
+/*   Avaj.java                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sscottie <sscottie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 13:56:40 by sscottie          #+#    #+#             */
-/*   Updated: 2021/03/13 13:56:50 by sscottie         ###   ########.fr       */
+/*   Updated: 2021/03/13 16:39:08 by sscottie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+public class Avaj {
+	private static int iterations;
+
+	private static void main (String[] args) {
+		if (args.length != 1) {
+			throw new IncorrectScenarioFileException("There is no scenario.txt file")
+		}
+		
+	}
+}
