59c335ec2c7ba3359bc5d9190afc9cf0e3ff82c2
Sherry Scottie <sscottie@ox-p5.msk.21-school.ru>
2021-03-13 17:12:04 +0300

    added launch.sh, added Loger on Avaj.java

--- launch.sh
+++ launch.sh
@@ -6,12 +6,13 @@
 #    By: sscottie <sscottie@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/03/13 16:41:23 by sscottie          #+#    #+#              #
-#    Updated: 2021/03/13 16:52:44 by sscottie         ###   ########.fr        #
+#    Updated: 2021/03/13 17:11:05 by sscottie         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
+./clear_class.sh
 find * -name "*.java" > sources.txt
 javac -sourcepath . @sources.txt
 cd src
--- src/avaj/Avaj.java
+++ src/avaj/Avaj.java
@@ -6,7 +6,7 @@
 /*   By: sscottie <sscottie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/13 13:56:40 by sscottie          #+#    #+#             */
-/*   Updated: 2021/03/13 16:58:09 by sscottie         ###   ########.fr       */
+/*   Updated: 2021/03/13 17:11:15 by sscottie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ import avaj.exceptions.NoFileException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 
 public class Avaj {
 	private static int iterations;
@@ -26,6 +31,23 @@ public class Avaj {
 		if (args.length != 1) {
 			throw new NoFileException("There is no scenario.txt file");
 		}
-		
+		logger();
+	}
+
+	public static void logger() {
+		Logger logger = Logger.getLogger("Logger");
+		try {
+			FileHandler fileHandler = new FileHandler("./../simulation.txt");
+			logger.addHandler(fileHandler);
+			fileHandler.setFormatter(new Formatter() {
+				@Override
+				public String format(LogRecord record) {
+					return record.getMessage() + "\n";
+				} 
+			});
+			logger.setUseParentHandlers(false);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
